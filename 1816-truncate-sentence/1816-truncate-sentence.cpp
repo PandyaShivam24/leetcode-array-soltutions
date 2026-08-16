c33385b70977a40707b14f0c4bdd85ea111b9207
Shivam Pandya <pandyashivam013@gmail.com>
2026-08-16 22:20:22 +0530

    Time: 0 ms (100%), Space: 8.8 MB (95.52%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.cpp
@@ -0,0 +1,20 @@
+class Solution{
+public:
+    string truncateSentence(string s,int k){
+        int count=0;
+        string ans="";
+        for(char x:s)
+        {
+            if(x==' ')
+            {
+                count++;
+            }
+            if(count==k)
+            {
+                break;
+            }
+            ans+=x;
+        }
+        return ans;
+    }
+};
