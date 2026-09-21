592311fe0d10ec7da10cc9b641381a388f404b54
Shivam Pandya <pandyashivam013@gmail.com>
2026-09-21 15:36:13 +0530

    Time: 0 ms (100%), Space: 9.7 MB (78.56%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -1,13 +1,13 @@
-class Solution{
+class Solution {
 public:
-    int reverseDegree(string s)
+    int reverseDegree(string s) 
     {
+        int n=s.size();
         int sum=0;
-        for(int j=0;j<s.size();j++)
+        for(int i=0;i<n;i++)
         {
-            int rev=26-(s[j]-'a');
-            sum+=rev*(j+1);
-        }
+            sum+=(26-(s[i]-'a'))*(i+1);
+        }    
         return sum;
     }
 };
